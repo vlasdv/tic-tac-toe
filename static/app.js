1cc9b0ecf6a0e7c3e641693b33e28bae935915b6
Dmitrii Vlasov <vlas.dv@gmail.com>
2024-01-04 19:24:34 +0100

    Add GameController factory, which provides playing game mechanics

--- static/app.js
+++ static/app.js
@@ -32,7 +32,7 @@ function Cell() {
   let value = 0;
 
   // Add player's mark to the cell
-  const addMark = (mark) => {
+  const addValue = (mark) => {
     value = mark;
   };
   
@@ -41,5 +41,45 @@ function Cell() {
   return {addMark, getValue};
 }
 
+function GameController(playerOneName = 'Human', playerTwoName = 'Computer') {
+  const field = Field();
 
+  const players = [
+    {
+      name: playerOneName,
+      mark: 'X'
+    },
+    {
+      name: playerTwoName,
+      mark: 'O'
+    }
+  ];
+
+  let activePlayer = players[0];
+  const switchPlayer = () => activePlayer = (activePlayer === players[0]) ? players[1] : players[0];
+  const getActivePlayer = () => activePlayer;
+  
+  // print current round:
+  // current field status
+  // player's name
+  const printNewRound = () => {
+    field.printField();
+    console.log(`${getActivePlayer}'s turn.`);
+  }
+  
+  // play round (row, column):
+  // add player's mark to chosen row:column
+  const playRound = (row, column) => {
+    console.log(`Adding ${activePlayer}'s mark to ${row}:${column}...`);
+    field.addMark(activePlayer.mark, row, column);    
+
+    switchPlayer();
+    printNewRound();
+  }
+
+  // Initial round play
+  printNewRound();
+
+  return {playRound, getActivePlayer};
+}
 
